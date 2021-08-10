0424cab1da5dcac3779a64f56a4a0b3ac58947c5
ouahid <ouahid.khelifi@gmail.com>
2021-08-10 11:17:20 +0200

    feat: handle alert notification commands

--- gravitee-cockpit-connectors-core/src/main/java/io/gravitee/cockpit/connectors/core/services/AlertManagerService.java
+++ gravitee-cockpit-connectors-core/src/main/java/io/gravitee/cockpit/connectors/core/services/AlertManagerService.java
@@ -17,13 +17,19 @@ package io.gravitee.cockpit.connectors.core.services;
 
 import io.gravitee.alert.api.trigger.Trigger;
 import io.gravitee.alert.api.trigger.TriggerProvider;
+import io.gravitee.alert.api.trigger.command.Command;
 import io.gravitee.cockpit.api.CockpitConnector;
 import io.gravitee.cockpit.api.command.CommandStatus;
+import io.gravitee.cockpit.api.command.alert.notification.AlertNotificationCommand;
+import io.gravitee.cockpit.api.command.alert.notification.AlertNotificationPayload;
 import io.gravitee.cockpit.api.command.alert.trigger.list.ListAlertTriggersCommand;
 import io.gravitee.cockpit.api.command.alert.trigger.list.ListAlertTriggersReply;
 import io.reactivex.Completable;
 import io.reactivex.Maybe;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -32,6 +38,7 @@ public class AlertManagerService implements InitializingBean {
 
     private final CockpitConnector cockpitConnector;
     private final TriggerProvider triggerProvider;
+    private Set<String> triggersId = new HashSet<>();
 
     public AlertManagerService(CockpitConnector cockpitConnector, TriggerProvider triggerProvider) {
         this.cockpitConnector = cockpitConnector;
@@ -42,7 +49,7 @@ public class AlertManagerService implements InitializingBean {
     public void afterPropertiesSet() {
         log.info("Starting alert manager service");
         cockpitConnector.registerOnReadyListener(
-            () ->
+            () -> {
                 triggerProvider.addListener(
                     (TriggerProvider.OnConnectionListener) () -> {
                         log.info("Connected to alerting system. Sync cockpit alert triggers...");
@@ -53,7 +60,14 @@ public class AlertManagerService implements InitializingBean {
                                 }
                             );
                     }
-                )
+                );
+
+                triggerProvider.addListener(
+                    (TriggerProvider.OnCommandListener) (Command command) -> {
+                        doOnCommand(command);
+                    }
+                );
+            }
         );
     }
 
@@ -72,6 +86,32 @@ public class AlertManagerService implements InitializingBean {
                 }
             )
             .doOnSuccess(triggers -> triggers.forEach(triggerProvider::register))
+            .map(triggers -> (triggersId = triggers.stream().map(Trigger::getId).collect(Collectors.toSet())))
             .ignoreElement();
+    }
+
+    private void doOnCommand(Command command) {
+        if (command instanceof io.gravitee.alert.api.trigger.command.AlertNotificationCommand) {
+            io.gravitee.alert.api.trigger.command.AlertNotificationCommand alertNotificationCommand = (io.gravitee.alert.api.trigger.command.AlertNotificationCommand) command;
+            if (triggersId.contains(alertNotificationCommand.getTrigger())) {
+                cockpitConnector
+                    .sendCommand(convert(alertNotificationCommand))
+                    .subscribe(
+                        reply -> {
+                            log.info("Alert notification command sent");
+                        }
+                    );
+            }
+        }
+    }
+
+    private AlertNotificationCommand convert(io.gravitee.alert.api.trigger.command.AlertNotificationCommand command) {
+        AlertNotificationCommand alertNotificationCommand = new AlertNotificationCommand();
+        AlertNotificationPayload payload = new AlertNotificationPayload();
+        payload.setTrigger(command.getTrigger());
+        payload.setMessage(command.getMessage());
+        payload.setTimestamp(command.getTimestamp());
+        alertNotificationCommand.setPayload(payload);
+        return alertNotificationCommand;
     }
 }
